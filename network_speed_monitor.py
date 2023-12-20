22b84971ef0aafc76d5dac0952e72a2af43b8548
Harindu Jayakody <randeepana@gmail.com>
2023-12-20 18:00:28 +0530

    added logs feature & Reset Timer.

--- network_speed_monitor.py
+++ network_speed_monitor.py
@@ -3,6 +3,8 @@ import psutil
 import time
 import pyfiglet
 
+LOG_FILE = "data_usage_log.txt"
+
 def get_network_stats(interval=1):
     network_stats1 = psutil.net_io_counters()
     time.sleep(interval)
@@ -26,15 +28,66 @@ def custom_heading(text):
     custom_figlet = pyfiglet.Figlet()
     return custom_figlet.renderText(text)
 
+def read_last_session_totals():
+    total_upload = 0
+    total_download = 0
+
+    if os.path.exists(LOG_FILE):
+        with open(LOG_FILE, "r") as log_file:
+            lines = log_file.readlines()
+            if lines:
+                last_line = lines[-1]
+                parts = last_line.split(',')
+                if len(parts) >= 2:
+                    total_upload_str = parts[0].split(':')[-1].strip().replace('MB', '')
+                    total_download_str = parts[1].split(':')[-1].strip().replace('MB', '')
+
+                    try:
+                        total_upload = float(total_upload_str)
+                        total_download = float(total_download_str)
+                    except ValueError as e:
+                        print(f"Error converting values to float: {e}")
+
+    return total_upload, total_download
+
+def reset_totals_if_new_day(last_reset_time, current_time):
+    last_reset_date = time.strftime("%Y-%m-%d", time.localtime(last_reset_time))
+    current_date = time.strftime("%Y-%m-%d", time.localtime(current_time))
+
+    return last_reset_date != current_date
+
+def reset_totals():
+    return 0, 0
+
+def log_data(total_upload, total_download, last_log_time):
+    current_time = time.time()
+    if current_time - last_log_time >= 3600:  # Log every 1 hour (3600 seconds)
+        timestamp = time.strftime("%Y-%m-%d %H:%M:%S")
+        log_entry = f"{timestamp} - Upload: {total_upload:.2f} MB, Download: {total_download:.2f} MB\n"
+
+        with open(LOG_FILE, "a") as log_file:
+            log_file.write(log_entry)
+
+        return current_time  # Update last log time
+
+    return last_log_time
+
 if __name__ == "__main__":
     interval = 1  # Measurement interval in seconds
-    total_upload = 0  # Initialize total_upload to 0
-    total_download = 0  # Initialize total_download to 0
+    total_upload, total_download = read_last_session_totals()
     last_update_time = time.time()
+    last_reset_time = time.time()
+    last_log_time = time.time()
 
     try:
         while True:
             current_time = time.time()
+
+            # Reset totals if it's a new day
+            if reset_totals_if_new_day(last_reset_time, current_time):
+                last_reset_time = current_time
+                total_upload, total_download = reset_totals()
+
             if current_time - last_update_time >= interval:
                 bytes_sent, bytes_received = get_network_stats(interval)
                 upload_speed, download_speed = calculate_network_speed(bytes_sent, bytes_received, interval)
@@ -55,6 +108,9 @@ if __name__ == "__main__":
                 print(f"\nMade with ❤️ by {styled_text('Harindu Jayakody', '1;35;40')}")
                 print(f"GitHub: {styled_text('https://github.com/harindujayakody/network-monitor', '1;36;40')}")
                 print("\nPress Ctrl+C to exit gracefully.")
+
+                # Log data
+                last_log_time = log_data(total_upload, total_download, last_log_time)
 
                 last_update_time = current_time
 
